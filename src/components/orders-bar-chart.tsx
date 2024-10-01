9905de6c57ef9e683941bcad4d797859bfb34376
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2024-10-01 21:41:41 +0100

    feat: renamed shadcn graph props

--- src/components/orders-bar-chart.tsx
+++ src/components/orders-bar-chart.tsx
@@ -21,20 +21,20 @@ import {
 } from '@/components/ui/chart';
 
 const chartData = [
-    { month: 'April', desktop: 186, mobile: 80, percent: 3 },
-    { month: 'May', desktop: 305, mobile: 200, percent: 5 },
-    { month: 'June', desktop: 237, mobile: 120, percent: 4 },
-    { month: 'July', desktop: 73, mobile: 190, percent: 2 },
-    { month: 'August', desktop: 209, mobile: 130, percent: 3 },
-    { month: 'September', desktop: 214, mobile: 140, percent: 8 },
+    { month: 'April', orders: 186, loyalty: 80, percent: 3 },
+    { month: 'May', orders: 305, loyalty: 200, percent: 5 },
+    { month: 'June', orders: 237, loyalty: 120, percent: 4 },
+    { month: 'July', orders: 73, loyalty: 190, percent: 2 },
+    { month: 'August', orders: 209, loyalty: 130, percent: 3 },
+    { month: 'September', orders: 214, loyalty: 140, percent: 8 },
 ];
 
 const chartConfig = {
-    desktop: {
+    orders: {
         label: 'Orders',
         color: 'hsl(var(--chart-1))',
     },
-    mobile: {
+    loyalty: {
         label: 'Loyalty Points',
         color: 'hsl(var(--chart-2))',
     },
@@ -74,13 +74,13 @@ export default function OrdersBarChart() {
                         />
                         <ChartLegend content={<ChartLegendContent />} />
                         <Bar
-                            dataKey='desktop'
-                            fill='var(--color-desktop)'
+                            dataKey='orders'
+                            fill='var(--color-orders)'
                             radius={4}
                         />
                         <Bar
-                            dataKey='mobile'
-                            fill='var(--color-mobile)'
+                            dataKey='loyalty'
+                            fill='var(--color-loyalty)'
                             radius={4}
                         />
                         <Bar
