c454ce80f3d58c08047c703353af30d1e9db22bb
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2024-10-08 16:12:58 +0100

    feat: day selection single

--- src/components/day-selection.tsx
+++ src/components/day-selection.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useMemo, useState } from 'react';
+import { DayPicker } from 'react-day-picker';
+
 type DaySelectionProps = {
     otherPeriod: string;
     handleSelectPeriod: (value: string) => void;
@@ -7,5 +10,48 @@ export default function DaySelection({
     otherPeriod,
     handleSelectPeriod,
 }: DaySelectionProps) {
-    return <div>Day Selection</div>;
+    const startDate = useMemo(() => new Date(2022, 7, 22), []);
+    const endDate = useMemo(() => new Date().getTime() + 86400000, []);
+
+    const [disabledDays, setDisabledDays] = useState([
+        { from: new Date(0), to: startDate }, // Disable all dates before startDate
+        { from: new Date(endDate), to: new Date(2100, 0, 1) }, // Disable all dates after today
+    ]);
+
+    useEffect(() => {
+        if (!otherPeriod) {
+            return;
+        }
+        const [day, month, year] = otherPeriod.split('/').map(Number);
+
+        setDisabledDays([
+            { from: new Date(0), to: startDate }, // Disable all dates before startDate
+            { from: new Date(endDate), to: new Date(2100, 0, 1) }, // Disable all dates after today
+            {
+                from: new Date(year, month - 1, day),
+                to: new Date(year, month - 1, day),
+            },
+        ]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [otherPeriod]);
+
+    return (
+        <div className='flex flex-col justify-center gap-2'>
+            <p>Day: </p>
+            <DayPicker
+                showOutsideDays={true}
+                mode='single'
+                disabled={disabledDays}
+                onDayClick={(day) => {
+                    handleSelectPeriod(day.toLocaleDateString());
+                }}
+                fixedWeeks
+                captionLayout='dropdown'
+                defaultMonth={new Date()}
+                startMonth={new Date(2022, 7)}
+                endMonth={new Date()}
+                className='rounded-md border'
+            />
+        </div>
+    );
 }
